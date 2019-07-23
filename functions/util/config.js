11e944856a64aeece4ad3e8a75c247b7741461ec
mavsiapo <msiapo23@gmail.com>
2019-07-23 22:58:13 +0800

    new commit for 23/7

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBuBYpg4S4ZyA9WGk7SoiY1TaE1Ezm8iO8",
+    authDomain: "social-app-ecb54.firebaseapp.com",
+    databaseURL: "https://social-app-ecb54.firebaseio.com",
+    projectId: "social-app-ecb54",
+    storageBucket: "social-app-ecb54.appspot.com",
+    messagingSenderId: "314927917247",
+    appId: "1:314927917247:web:937e386225600418"
+};
